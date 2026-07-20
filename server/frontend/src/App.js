a75894a9e69dde3904057462e16cf1836b1bdaf2
Kuldeep Chudasama <kuldeepchudasama6999@gmail.com>
2026-07-20 03:28:14 -0400

    Module 4 Error

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,13 +1,16 @@
 import LoginPanel from "./components/Login/Login.jsx";
 import Register from "./components/Register/Register.jsx";
+import Dealers from "./components/Dealers/Dealers.jsx";
+
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<LoginPanel />} />
-      <Route path="/login" element={<LoginPanel />} />
-      <Route path="/register" element={<Register />} />
+        <Route path="/" element={<LoginPanel />} />
+        <Route path="/login" element={<LoginPanel />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dealers" element={<Dealers/>} />
     </Routes>
   );
 }
