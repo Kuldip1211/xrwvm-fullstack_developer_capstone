2c1090b96f4d2d8f1596d8cb7c1a5cd6d51168ca
Kuldeep Chudasama <kuldeepchudasama6999@gmail.com>
2026-07-20 06:50:41 -0400

    Review Post

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,6 +1,8 @@
 import LoginPanel from "./components/Login/Login.jsx";
 import Register from "./components/Register/Register.jsx";
 import Dealers from "./components/Dealers/Dealers.jsx";
+import Dealer from "./components/Dealers/Dealer.jsx";
+import PostReview from "./components/Dealers/PostReview.jsx";
 
 import { Routes, Route } from "react-router-dom";
 
@@ -11,6 +13,8 @@ function App() {
         <Route path="/login" element={<LoginPanel />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dealers" element={<Dealers/>} />
+        <Route path="/dealer/:id" element={<Dealer/>} />
+        <Route path="/postreview/:id" element={<PostReview/>} />
     </Routes>
   );
 }
